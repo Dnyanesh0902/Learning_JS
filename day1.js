f1c1e4e835439e44b98ba5e5441798685f1a1e29
dnyaneshwarkokate <Dnyaneshwarkokate@@electropneumatics.com>
2026-02-25 12:36:18 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,60 @@
+// program 1 - Even Or Odd
+let num = 59;
+if(num % 2 === 0)
+{
+    console.log("Even");
+}
+else
+{
+     console.log("Odd");
+}
+// Program 2 – Largest of Two Numbers
+
+let x = 200;
+let y = 35
+if(x > y)
+{
+    console.log("X is Greater Than Y");
+    
+}
+else
+{
+    console.log("Y is Greater Than X");
+    
+}
+// Program 3 – Grade System
+
+let marks = 85;
+
+if(marks >= 90){
+    console.log("A Grade");
+    
+}
+else if(marks >= 70)
+{
+    console.log("B Grade");
+}
+else
+{
+     console.log("C Grade");
+}
+
+// Home Work
+
+let nums = 15;
+if(nums % 3=== 0 && nums % 5 === 0)
+{
+    console.log("FizzBuzz");
+}
+else if(nums % 3=== 0)
+{
+    console.log("Fizz");
+}
+else if(nums % 5=== 0)
+{
+    console.log("Buzz");
+}
+else
+{
+      console.log("Not divisible by 3 or 5");
+}
