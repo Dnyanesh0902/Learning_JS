c7961269d84fe2943852872ea8497a69f5151a59
dnyaneshwarkokate <Dnyaneshwarkokate@@electropneumatics.com>
2026-02-27 12:04:46 +0530

    day2

--- day2.js
+++ day2.js
@@ -1,10 +1,10 @@
-var a = 10;
-var b = 20;  // allowed
+let a = 10;
+let b = 25;
+let c = 15;
 console.log(a);
 
 // Check even or odd number.
 
-var c = 23;
 
 if(c % 2 === 0)
 {
@@ -17,8 +17,29 @@ else
 
 // Check largest of 3 numbers.
 
-if(a > b)
+if(a > b && a > c)
 {
-    console.log("A is Greater than B");
+    console.log("A is largest :", a);
+    
+} else if(b > a &&  b > c)
+{
+    console.log("B is Largest :", b);
     
 }
+else{
+    console.log("C Is Largest :", c);
+    
+}
+//Check if number divisible by both 3 and 5.
+if(a % 3 === 0 && a % 5 ===0)
+{
+console.log("Number is divisible by both 3 and 5");    
+}
+
+console.log(5 + "5");
+console.log("5" - 2);
+console.log(true + 1);
+
+console.log([] + []);
+console.log([] + {});
+console.log({} + []);
